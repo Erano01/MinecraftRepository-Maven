337d67619b9f8db559548461f265620f0cd52a7c
rokel <muhammedalisert01@gmail.com>
2024-11-02 20:29:43 +0300

    Menu feature push

--- MinecraftPlugins/EranoAPI/Core/src/main/java/me/erano/com/core/menu/Component.java
+++ MinecraftPlugins/EranoAPI/Core/src/main/java/me/erano/com/core/menu/Component.java
@@ -1,11 +1,16 @@
 package me.erano.com.core.menu;
 
+import org.bukkit.inventory.ItemStack;
+
 //component element of composite pattern for menu feature
 public interface Component {
 
     void draw();
-    void add(Component component);
-    void remove(Component component);
+    void add(int index,Component component);
+    void remove(int index);
     Component getChildren(int index);
+    String getName();
+    ItemStack getIcon();
+
 
 }
--- MinecraftPlugins/EranoAPI/Core/src/main/java/me/erano/com/core/menu/Menu.java
+++ MinecraftPlugins/EranoAPI/Core/src/main/java/me/erano/com/core/menu/Menu.java
@@ -1,5 +1,6 @@
 package me.erano.com.core.menu;
 
+import org.bukkit.Material;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.inventory.InventoryCloseEvent;
 import org.bukkit.event.inventory.InventoryOpenEvent;
@@ -14,31 +15,47 @@ import java.util.Map;
 //composite element of composite pattern for menu feature
 public abstract class Menu implements Component{
 
-    //private Inventory inventory;
-
+    private Inventory inventory;
     private ItemStack icon;
     private String name;
-    private final List<Component> componentList = new ArrayList<Component>();
-    private final Map<Integer, Component> iconMap = new HashMap<>();
+    private final Map<Integer, Component> componentMap = new HashMap<>();
 
-    @Override
-    public void draw() {
-
+    public Menu(String name, ItemStack icon) {
+        this.name = name;
+        this.icon = icon;
     }
 
     @Override
-    public void add(Component component) {
-        componentList.add(component);
+    public void draw(){
+        for (int i = 0; i < componentMap.size(); i++) {
+            Component component = componentMap.get(i);
+            inventory.setItem(i, component.getIcon());
+        }
     }
 
     @Override
-    public void remove(Component component) {
-        componentList.remove(component);
+    public void add(int index, Component component) {
+        componentMap.put(index, component);
+    }
+
+    @Override
+    public void remove(int index) {
+        componentMap.remove(index);
     }
 
     @Override
     public Component getChildren(int index) {
-        return componentList.get(index);
+        return componentMap.get(index);
+    }
+
+    @Override
+    public String getName() {
+        return this.name;
+    }
+
+    @Override
+    public ItemStack getIcon() {
+        return this.icon;
     }
 
     abstract void onClick(InventoryClickEvent event);
